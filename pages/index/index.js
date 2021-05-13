36e7d70e3183a6bdf3c4ee2a78272f564ddbc965
meng <1059093241@qq.com>
2021-05-13 13:41:58 +0800

    基本功能完成，还可以优化

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,45 +4,32 @@ const app = getApp()
 
 Page({
   data: {
-    motto: '你好',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo'),
-    canIUseGetUserProfile: false,
-    canIUseOpenData: wx.canIUse('open-data.type.userAvatarUrl') && wx.canIUse('open-data.type.userNickName') // 如需尝试获取用户信息可改为false
+    index1: 0,
+    index2: 0,
+    message: "?",
+    arrayX: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+    arrayY: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+    number: 0
   },
-  // 事件处理函数
-  bindViewTap() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
+  createNumber: function () {
+    let value = parseInt(100 * Math.random());
+    this.number = value;
   },
-  onLoad() {
-    if (wx.getUserProfile) {
-      this.setData({
-        canIUseGetUserProfile: true
-      })
-    }
-  },
-  getUserProfile(e) {
-    // 推荐使用wx.getUserProfile获取用户信息，开发者每次通过该接口获取用户个人信息均需用户确认，开发者妥善保管用户快速填写的头像昵称，避免重复弹窗
-    wx.getUserProfile({
-      desc: '展示用户信息', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
-      success: (res) => {
-        console.log(res)
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    })
-  },
-  getUserInfo(e) {
-    // 不推荐使用getUserInfo获取用户信息，预计自2021年4月13日起，getUserInfo将不再弹出弹窗，并直接返回匿名的用户个人信息
-    console.log(e)
+  bindPickerChange: function (e) {
     this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
+      index1: e.detail.value
     })
+    this.index1 = e.detail.value
+  },
+  bindPickerChangeY: function (e) {
+    this.setData({
+      index2: e.detail.value
+    })
+    this.index2 = e.detail.value
+  },
+  ok: function () {
+    let sum = parseInt(this.index1) * 10 + parseInt(this.index2)
+    if (sum > this.number) { wx.showModal({ content: '大了' }) }
+    else if (sum < this.number) { wx.showModal({ content: '小了' }); } else { wx.showModal({ content: '正确' }); }
   }
 })
